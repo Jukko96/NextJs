c44386c2d900aae20e8cde2ad97bb3224a7eb7d1
Jukko96 <cslzh4433@gmail.com>
2025-03-27 23:37:14 +0900

    add usePathname

---
+++ app/(beforeLogin)/extra/page.tsx
@@ -0,0 +1,7 @@
+const Page = () =>{
+    return <div>
+        여기선 추가 메뉴가 보입니다.
+    </div>
+}
+
+export default Page;
--- app/(beforeLogin)/layout.tsx
+++ app/(beforeLogin)/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import style from './page.module.css'
 import NavMenu from "../_common/_components/NavMenu";
+import { usePathname } from "next/navigation";
 
 const Before_layout = ({
     children,
@@ -9,12 +10,17 @@ const Before_layout = ({
     children : ReactNode,
     modal : ReactNode,
 }) => {
+    const path_name = usePathname();
 
     return <div className={style.container}>
         <section className={style.sidebar}>
           <NavMenu url={"/home"} text={"홈"}/>
           <NavMenu url={"/signin"} text={"로그인"}/>
           <NavMenu url={"/signup"} text={"회원가입"}/>
+          {path_name !== "/signin" && (
+            <NavMenu url="/extra" text="추가 메뉴" />
+            )}
+
         </section>
         {children}
         {modal}
